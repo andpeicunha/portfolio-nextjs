3f6011b9a81e61afef817ed6f7225750839f7586
André Peixoto <andrepeixoto@gmail.com>
2023-01-22 14:49:35 -0300

    include nav bar

--- app/navigator/page.tsx
+++ app/navigator/page.tsx
@@ -1,4 +1,4 @@
-//PAGINA LAYTOU NAVEGAÇÃO PRINCIPAL
+//NAVEGAÇÃO
 "use client";
 
 import { motion } from "framer-motion";
@@ -8,7 +8,7 @@ export default function Nav() {
   return (
     <div
       id="Nav"
-      className="fixed w-full h-[50px] top-0 bg-gray-800 py-2 pr-3 sm:pr-6"
+      className="relative z-20 w-full h-[50px] top-0 bg-gray-800 py-2 pr-3 sm:pr-6"
     >
       <div className="flex justify-start items-center w-full space-x-4 p-1 ml-2 pr-3">
         <Logo className="grow aspect-auto" width={30} />
