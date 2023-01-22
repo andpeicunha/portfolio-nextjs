3384a089dd036d047de83d48eb342a328bf70aad
André Peixoto <andrepeixoto@gmail.com>
2023-01-22 14:49:50 -0300

    include tsparticles

---
+++ app/particles/particles.js
@@ -0,0 +1,109 @@
+//PARTICLES JS
+"use client";
+
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+
+const ParticleLogo = () => {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
+    return (
+        <div className="absolute top-0 -z-50">
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    background: {
+                        color: {
+                            value: "#000",
+                        },
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",// grab
+                                "parallax": {
+                                    "enable": false,
+                                    "force": 150,
+                                    "smooth": 100
+                                }
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 80,
+                                duration: 0.3,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#272727",
+                        },
+                        links: {
+                            color: "#272727",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            directions: "none",
+                            enable: true,
+                            outModes: {
+                                default: "none",
+                            },
+                            random: true,
+                            speed: 1,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 700,
+                            },
+                            value: 150,
+                        },
+                        opacity: {
+                            value: 0.1,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 5 },
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
+        </div>
+    );
+};
+
+export default ParticleLogo;
