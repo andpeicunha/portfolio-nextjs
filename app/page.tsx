57e563e0ed8c8e47c77e3ce524a4a02a010eaaa5
André Peixoto <andrepeixoto@gmail.com>
2023-01-22 14:50:33 -0300

    include layout in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Estou na Home</div>;
+  return (
+    <div className="relative z-10 h-full top-0 text-white text-5xl bg-red-300">
+      Body Home
+    </div>
+  );
 }
